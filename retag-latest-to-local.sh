5f8a3ba57839c2c165471f0e8a590e1efea9031e
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-09-04 21:24:09 +0200

    build dockerfiles without compose because it failed

--- retag-latest-to-local.sh
+++ retag-latest-to-local.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 for image_id in `docker images | grep ubuntu-latest | awk '{print $3}'`; do
-  image_name=`docker images | grep $image_id | awk '{print $1}'`
-  docker tag $image_name:ubuntu-latest $image_name:ubuntu-local
+    image_name=`docker images | grep ubuntu-latest | grep $image_id | awk '{print $1}'`
+    #echo image_name=$image_name image_id=$image_id
+    docker tag $image_name:ubuntu-latest $image_name:ubuntu-local
 done
 
-docker images | grep ubuntu
+docker images | grep ubuntu-l
+
