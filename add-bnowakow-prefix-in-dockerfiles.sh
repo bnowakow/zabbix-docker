6f2b54ac6933492c5360ba25553a6abc4044616b
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-09-11 16:47:13 +0200

    update images in docker compose file to have bnowakow/ prefix

--- add-bnowakow-prefix-in-dockerfiles.sh
+++ add-bnowakow-prefix-in-dockerfiles.sh
@@ -3,10 +3,13 @@
 for file in `find Dockerfiles -name Dockerfile`; do
     echo "$file"
     head -n 5 "$file"
-#    gsed 's/\(ARG\ BUILD_BASE_IMAGE=\)/\1bnowkow\//' "$file" | head -n 5
+#    gsed 's/\(ARG\ BUILD_BASE_IMAGE=\)/\1bnowakow\//' "$file" | head -n 5
     gsed -i 's/\(ARG\ BUILD_BASE_IMAGE=\)/\1bnowakow\//' "$file"
     head -n 5 "$file"
     echo
 #    exit
 done
+
+#gsed 's/\(image: \)zabbix/\1bnowakow\/zabbix/' "docker-compose_v3_ubuntu_mysql_local.yaml" | head -n 10
+gsed -i 's/\(image: \)zabbix/\1bnowakow\/zabbix/' "docker-compose_v3_ubuntu_mysql_local.yaml"
 
